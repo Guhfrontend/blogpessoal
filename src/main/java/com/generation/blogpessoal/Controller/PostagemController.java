cadf9d4007fb0ed689a3efeb457dd1499518994c
Gustavo Ribeiro <gustavo.rsilva08@gmail.com>
2024-07-02 19:29:32 -0300

    create: ENUM and metods

--- src/main/java/com/generation/blogpessoal/Controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/Controller/PostagemController.java
@@ -5,7 +5,6 @@ import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
